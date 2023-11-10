d348b97d98e188300daa4087d55d486fa8032fe6
ZAIDs07 <148442351+ZAIDs07@users.noreply.github.com>
2023-11-10 12:10:32 +0530

    Add files via upload

---
+++ HTTP Requests_Git/index.js
@@ -0,0 +1,19 @@
+import express from "express";
+const app=express();
+const port=3000;
+
+app.get("/",(req,res)=>{
+    res.send("<h1>Hello, World!<h1/>")
+})
+
+app.get("/about", (req,res)=>{
+    res.send("<h1>I am the God of the new world!<h1/>")
+})
+
+app.get("/contact", (req,res)=>{
+    res.send("<h1>Dont<h1/>");
+})
+
+app.listen(port,()=>{
+    console.log(`Your server is running on port ${port}.`)
+})
